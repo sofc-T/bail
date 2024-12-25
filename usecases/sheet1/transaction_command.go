7d33a6d1cf0e9589fbfdaa22440a957d17865727
Sofonyas Tizazu <sofstzz@gmail.com>
2024-12-25 22:50:41 +0300

    tansaction data

--- usecases/sheet1/transaction_command.go
+++ usecases/sheet1/transaction_command.go
@@ -2,14 +2,14 @@ package transaction_cmd
 
 type Sheet1Command struct {
 	file []byte 
-	sheetname string 
+	sheet int 
 }
 
 // NewLoginCommand creates a new instance of LoginCommand.
-func NewLoginCommand(email, password string) *Sheet1Command {
+func NewLoginCommand(file []byte, sheet int) *Sheet1Command {
 	return &Sheet1Command{
-		email: email,
-		password: password,
+		file: file,
+		sheet: sheet,
 	}
 }
 
